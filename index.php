95a1c8cfc6644307d9be22302be9e23b2b70b186
Matt Jenkins <matthew.jenkins@my.maryvillecollege.edu>
2016-04-06 16:46:06 -0400

    Wireframes and login page
    Login page created
    Wireframes added
    Updated logo

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+  <title>MC CoreDB Login</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css"  />
+</head>
+<body>
+  <div id="wrapper">
+    <div id="login_wrapper">
+      <img class="login_logo" src="img/logo_white.svg" alt="MC CoreDB" />
+      <input id="username" class="login_input" name="username" type="text" placeholder="Username" />
+      <input id="password" class="login_input" name="password" type="password" placeholder="Password"/>
+      <div id="buttons" class="center">
+        <button id="create_user" class="button secondary_button" name="create_user">Register</button>
+        <button id="submit_login" class="button orange_button login_button" name="submit_login">Login</button>
+      </div>
+    </div>
+  </div>
+</body>
+<html>
