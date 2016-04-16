1a615d143fa556cbb6310f44fd844bdbfbb6bb15
Matt Jenkins <matthew.jenkins@my.maryvillecollege.edu>
2016-04-16 14:38:28 -0400

    Syncing new logos and small addition to index back to server

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+  session_start();
+?>
+
 <!doctype html>
 <html>
 <head>
@@ -8,12 +12,14 @@
   <div id="wrapper">
     <div id="login_wrapper">
       <img class="login_logo" src="img/logo_white.svg" alt="MC CoreDB" />
-      <input id="username" class="login_input" name="username" type="text" placeholder="Username" />
-      <input id="password" class="login_input" name="password" type="password" placeholder="Password"/>
-      <div id="buttons" class="center">
-        <button id="create_user" class="button secondary_button" name="create_user">Register</button>
-        <button id="submit_login" class="button orange_button login_button" name="submit_login">Login</button>
-      </div>
+      <form id="user_login_form">
+        <input id="username" class="login_input" name="username" type="text" placeholder="Username" />
+        <input id="password" class="login_input" name="password" type="password" placeholder="Password"/>
+        <div id="buttons" class="center">
+          <button id="create_user" class="button secondary_button" name="create_user">Register</button>
+          <button id="submit_login" class="button orange_button login_button" name="submit_login">Logindc</button>
+        </div>
+      </form>
     </div>
   </div>
 </body>
